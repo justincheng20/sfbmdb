a60217401dca51234a7e7930d0ddc2852825f819
justincheng20 <justincheng20@gmail.com>
2020-07-08 15:59:14 -0700

    init commit

---
+++ server/api/api.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+let data = [];
+
+app.get('/', async function(req, res, next) {
+  try {
+    return res.status(200).json({ data });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+app.post('/', async function(req, res, next) {
+  try {
+    const { item } = { ...req.body };
+    if (item.trim().length === 0) throw new Error('error');
+    data = [item, ...data];
+    return res.status(201).json({ item });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+module.exports = app;
