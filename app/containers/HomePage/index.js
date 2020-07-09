6b35bffaa66c026876f8b5b64b3eb6d7c29e82d5
justincheng20 <justincheng20@gmail.com>
2020-07-09 15:42:50 -0700

    set up homepage skeleton

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -5,14 +5,78 @@
  *
  */
 
-import React from 'react';
+import React, { useEffect, memo } from 'react';
+import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { createStructuredSelector } from 'reselect';
+import { useInjectSaga } from 'utils/injectSaga';
+import {
+  makeSelectData,
+  makeSelectLoading,
+  makeSelectError,
+} from 'containers/App/selectors';
 import messages from './messages';
+import { loadItems } from '../App/actions';
+import saga from './saga';
 
-export default function HomePage() {
+const key = 'home';
+
+export function HomePage({ loading, error, onLoad, data }) {
+  useInjectSaga({ key, saga });
+
+  useEffect(() => {
+    if (!data.items) onLoad();
+  }, []);
+
+  const itemsListProps = {
+    data,
+    loading,
+    error,
+  };
+
+  if (loading) {
+    return 'Loading...';
+  }
+
+  if (error) {
+    return 'Something went wrong.';
+  }
+
   return (
     <h1>
       <FormattedMessage {...messages.header} />
     </h1>
   );
 }
+
+HomePage.propTypes = {
+  loading: PropTypes.bool,
+  error: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
+  data: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
+  onLoad: PropTypes.func,
+};
+
+const mapStateToProps = createStructuredSelector({
+  data: makeSelectData(),
+  loading: makeSelectLoading(),
+  error: makeSelectError(),
+});
+
+export function mapDispatchToProps(dispatch) {
+  return {
+    onLoad: () => dispatch(loadItems()),
+  };
+}
+
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+);
+
+export default compose(
+  withConnect,
+  memo,
+)(HomePage);
+
